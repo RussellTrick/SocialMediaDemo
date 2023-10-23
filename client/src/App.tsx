0aa11c61a46ce53880f21d521c04ec9a2cc7153b
Owain <owain.evans@hotmail.co.uk>
2023-10-23 22:38:18 +0100

    Basic routing, sass and fontawesome install

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./styles/App.css";
+import React from "react";
+import "./styles/App.scss";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import TopNavBar from "./components/navbar/TopNavbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+      </Route>
+    )
+  );
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </>
   );
 }
+
+const Root = () => {
+  return (
+    <>
+      <TopNavBar />
+      <Outlet />
+    </>
+  );
+};
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
