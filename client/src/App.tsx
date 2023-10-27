4641bc6d8ee3239888c3ae5db4e7972d43c327c2
Owain <owain.evans@hotmail.co.uk>
2023-10-27 01:33:00 +0100

    routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./styles/App.scss";
 import {
   createBrowserRouter,
@@ -9,12 +8,32 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home";
 import TopNavBar from "./components/navbar/TopNavbar";
+import Profiles from "./pages/Profiles";
+import Awards from "./pages/Awards";
+import Achievements from "./pages/Achievements";
+import Profile from "./pages/Profile";
+import Favourites from "./pages/Favourites";
+import Gems from "./pages/Gems";
+import Messenger from "./pages/Messenger";
+import Tags from "./pages/Tags";
+import Clips from "./pages/Clips";
+import Cart from "./pages/Cart";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
         <Route index element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profiles" element={<Profiles />} />
+        <Route path="/achievements" element={<Achievements />} />
+        <Route path="/awards" element={<Awards />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="/gems" element={<Gems />} />
+        <Route path="/messenger" element={<Messenger />} />
+        <Route path="/tags" element={<Tags />} />
+        <Route path="/clips" element={<Clips />} />
+        <Route path="/cart" element={<Cart />} />
       </Route>
     )
   );
